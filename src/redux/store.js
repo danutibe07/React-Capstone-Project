78808e29547809d6063fa61f03d656e8fe7c734d
danutibe07 <danielutibe07@gmail.com>
2023-04-23 12:21:13 +0100

    redux state and fetching  data from api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cryptoReducer from './crypto/cryptoSlice';
 
 const store = configureStore({
   reducer: {
-
+    crypto: cryptoReducer,
   },
 });
 
